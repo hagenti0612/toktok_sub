56cd9cbed66f38db9383429bf03b8ec2f9a7968d
unknown <ssl82@naver.com>
2024-11-26 03:32:37 +0900

    'test23'

--- src/env/socket.js
+++ src/env/socket.js
@@ -1,11 +1,6 @@
 import { io } from "socket.io-client";
 
-// 단일 소켓 인스턴스 생성
-let socket;
+const socket = io('https://substantial-adore-imds-2813ad36.koyeb.app', { secure: true });
 
-export const getSocket = () => {
-  if (!socket) {
-    const socket = io('https://substantial-adore-imds-2813ad36.koyeb.app', { secure: true });
-  }
-  return socket;
-};
+export default socket;
+
